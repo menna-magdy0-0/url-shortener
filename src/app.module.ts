8c0cbfa5e2afce353eb609d1d4ea5414ec8d0133
Menna Magdy <mennamagdy4291@gmail.com>
2025-11-28 22:22:21 +0200

    Added Cache using radis logic to service URL

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,22 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UrlsModule } from './urls/urls.module';
 import { ConfigModule } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-ioredis-yet';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    CacheModule.registerAsync({
+      useFactory: () => ({
+        isGlobal: true,
+        store: redisStore,
+        host: 'localhost',
+        port: 6379,
+        ttl: 60*60, // 1 hour
+      }),
     }),
     PrismaModule,
      UrlsModule
--- src/urls/urls.service.ts
+++ src/urls/urls.service.ts
@@ -1,20 +1,29 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import type { Cache } from 'cache-manager';
 import { CreateUrlDto } from './dto/create-url.dto';
 import { UpdateUrlDto } from './dto/update-url.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { nanoid } from 'nanoid';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
 @Injectable()
 export class UrlsService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService ,
+     @Inject(CACHE_MANAGER) private cacheManager:Cache
+    ) {}
   async create(createUrlDto: CreateUrlDto) {
   const shortCode = nanoid(10);
 
-    return await this.prisma.shortUrl.create({
+    const createdUrl = await this.prisma.shortUrl.create({
       data: {
         shortCode,
         url: createUrlDto.url,
       },
+      
     });
+    await this.cacheManager.set(`url-${shortCode}`, createdUrl);
+    console.log('Cache hit after create:', await this.cacheManager.get(`url-${shortCode}`));
+    return createdUrl;
   }
 
   findAll() {
@@ -22,21 +31,35 @@ export class UrlsService {
   }
 
   async findOne(shortCode: string) {
-    const record = await this.prisma.shortUrl.findUnique({ where: { shortCode } });
+    // Try to get url data from cache first
+    const cachedUrl = await this.cacheManager.get(`url-${shortCode}`);
+    if (cachedUrl) {
+      console.log('Cache hit:', cachedUrl);
+      return cachedUrl;
+    }
+    console.log('Cache miss for shortCode:', shortCode);
+    // If not found in cache, get from database
+    const url = await this.prisma.shortUrl.findUnique({ where: { shortCode } });
+    
+    //if not found in db
+    if (!url) throw new NotFoundException('Short URL not found');
+    // Store in cache for next time
+    await this.cacheManager.set(`url-${shortCode}`, url);
 
-    if (!record) throw new NotFoundException('Short URL not found');
-
-    return record;
+    return url;
   }
 
   async update(shortCode: string, updateUrlDto: UpdateUrlDto) {
     try {
-      return await this.prisma.shortUrl.update({
+      const updatedUrl = await this.prisma.shortUrl.update({
         where: { shortCode },
         data: {
           url: updateUrlDto.url,
         },
       });
+      await this.cacheManager.set(`url-${shortCode}`, updatedUrl);
+      console.log('Cache updated after update:', await this.cacheManager.get(`url-${shortCode}`));
+      return updatedUrl;
     } catch {
       throw new NotFoundException('Short URL not found');
     }
@@ -45,6 +68,8 @@ export class UrlsService {
   async remove(shortCode: string) {
     try {
       await this.prisma.shortUrl.delete({ where: { shortCode } });
+      await this.cacheManager.del(`url-${shortCode}`);
+      console.log('Cache miss');
       return;
     } catch {
       throw new NotFoundException('Short URL not found');
