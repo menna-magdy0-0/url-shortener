61d751908dc39eb4bceea945ccf34f1f7424188b
Menna Magdy <mennamagdy4291@gmail.com>
2025-11-29 18:17:49 +0200

    use redis cache instead of inmemory cache

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,20 +6,26 @@ import { UrlsModule } from './urls/urls.module';
 import { ConfigModule } from '@nestjs/config';
 import { CacheModule } from '@nestjs/cache-manager';
 import * as redisStore from 'cache-manager-ioredis-yet';
-
+import KeyvRedis from '@keyv/redis';
+import { Keyv } from 'keyv';
+import { CacheableMemory } from 'cacheable';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    CacheModule.registerAsync({
+   CacheModule.registerAsync({
       isGlobal: true,
-      useFactory: () => ({
-        store: redisStore,
-        host: 'localhost',
-        port: 6379,
-        ttl: 3600* 1000, 
-      }),
+      useFactory: async () => {
+        return {
+          stores: [
+            new Keyv({
+              store: new CacheableMemory({ ttl: 3600_000, lruSize: 5000 }),
+            }),
+            new KeyvRedis('redis://localhost:6379'),
+          ],
+        };
+      },
     }),
     PrismaModule,
      UrlsModule
